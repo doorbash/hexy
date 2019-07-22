55b46438b0624efa1c91a18229121f0cfbbf4fb2
Milad Doorbash <milad.doorbash@gmail.com>
2019-07-23 02:35:35 +0430

    add color util

---
+++ core/src/ir/doorbash/hexy/util/ColorUtil.java
@@ -0,0 +1,16 @@
+package ir.doorbash.hexy.util;
+
+/**
+ * Created by Milad Doorbash on 7/22/2019.
+ */
+public class ColorUtil {
+    public static final Color4[] color_index_to_rgba = new Color4[]{
+            new Color4(0.129f, 0.588f, 0.952f, 1.0f),
+            new Color4(0.474f, 0.333f, 0.282f, 1.0f),
+            new Color4(0.298f, 0.686f, 0.313f, 1.0f),
+            new Color4(1.0f, 0.596f, 0.0f, 1.0f),
+            new Color4(0.611f, 0.152f, 0.690f, 1.0f),
+            new Color4(0.956f, 0.262f, 0.211f, 1.0f),
+            new Color4(0.247f, 0.317f, 0.709f, 1.0f),
+    };
+}
