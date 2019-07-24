dc02e7dc36d5d0cb333d63f6abaacc6e60371f49
Milad Doorbash <milad.doorbash@gmail.com>
2019-07-24 19:31:16 +0430

    add splash screen

--- core/src/ir/doorbash/hexy/HexyGame.java
+++ core/src/ir/doorbash/hexy/HexyGame.java
@@ -1,15 +1,33 @@
 package ir.doorbash.hexy;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 
 public class HexyGame extends Game {
 
-    PlayScreen playScreen;
+    private static final boolean SHOW_SPLASH = true;
+
+    private SplashScreen splashScreen;
+    private PlayScreen playScreen;
 
     @Override
     public void create() {
+        splashScreen = new SplashScreen();
         playScreen = new PlayScreen();
-        this.setScreen(playScreen);
+        if (SHOW_SPLASH) {
+            new Thread(() -> {
+                Gdx.app.postRunnable(() -> setScreen(splashScreen));
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Gdx.app.postRunnable(() -> setScreen(playScreen));
+                }
+            }).start();
+        } else {
+            setScreen(playScreen);
+        }
     }
 
 }
---
+++ core/src/ir/doorbash/hexy/SplashScreen.java
@@ -0,0 +1,59 @@
+package ir.doorbash.hexy;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class SplashScreen implements Screen {
+    private SpriteBatch batch;
+    private Sprite splashSprite;
+    private Texture splashTexture;
+
+    SplashScreen() {
+        super();
+        batch = new SpriteBatch();
+        splashTexture = new Texture(Gdx.files.internal("splash.png"), true);
+        splashTexture.setFilter(Texture.TextureFilter.MipMapLinearLinear, Texture.TextureFilter.Linear);
+        splashSprite = new Sprite(splashTexture);
+        splashSprite.setSize(Gdx.graphics.getWidth() / 2f, Gdx.graphics.getWidth() / 2f);
+        splashSprite.setCenter(Gdx.graphics.getWidth() / 2f, Gdx.graphics.getHeight() / 2f);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1.0f, 1.0f, 1.0f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        splashSprite.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void dispose() {
+        splashTexture.dispose();
+        batch.dispose();
+    }
+}
