924163e0df15ff052201e80994e6a6582205cf07
Milad Doorbash <milad.doorbash@gmail.com>
2019-07-30 04:32:13 +0430

    cleanup

--- core/src/ir/doorbash/hexy/HexyGame.java
+++ core/src/ir/doorbash/hexy/HexyGame.java
@@ -12,7 +12,6 @@ public class HexyGame extends Game {
 
     @Override
     public void create() {
-        SimpleMesh.init();
         TrailGraphic.init();
 
         splashScreen = new SplashScreen();
