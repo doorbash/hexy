1ac84a6f9f737f53ef239bb3c7f2029592e7e509
Milad Doorbash <milad.doorbash@gmail.com>
2019-07-30 04:33:00 +0430

    change desktop resolution to 360x600

--- desktop/src/ir/doorbash/hexy/desktop/DesktopLauncher.java
+++ desktop/src/ir/doorbash/hexy/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ import ir.doorbash.hexy.HexyGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 480;
-		config.height = 800;
+		config.width = 360;
+		config.height = 600;
 		new LwjglApplication(new HexyGame(), config);
 	}
 }
