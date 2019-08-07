f11295d4f0ba965e23f15104a0fbeb3c30916554
Milad Doorbash <milad.doorbash@gmail.com>
2019-08-08 01:27:31 +0430

    portrait -> landscape

--- desktop/src/ir/doorbash/hexy/desktop/DesktopLauncher.java
+++ desktop/src/ir/doorbash/hexy/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ import ir.doorbash.hexy.HexyGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 480;
-		config.height = 800;
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new HexyGame(), config);
 	}
 }
