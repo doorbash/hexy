bfc278c667fdbc4022bd4154a0a361544cc2c622
Milad Doorbash <milad.doorbash@gmail.com>
2019-08-14 01:11:08 +0430

    add _clone method

--- core/src/ir/doorbash/hexy/model/MyState.java
+++ core/src/ir/doorbash/hexy/model/MyState.java
@@ -12,25 +12,39 @@ import io.colyseus.serializer.schema.annotations.SchemaField;
 
 @SchemaClass
 public class MyState extends Schema {
-	@SchemaField("0/map/ref")
-	public MapSchema<Cell> cells = new MapSchema<>(Cell.class);
+    @SchemaField("0/map/ref")
+    public MapSchema<Cell> cells = new MapSchema<>(Cell.class);
 
-	@SchemaField("1/map/ref")	
-	public MapSchema<Player> players = new MapSchema<>(Player.class);
+    @SchemaField("1/map/ref")
+    public MapSchema<Player> players = new MapSchema<>(Player.class);
 
-	@SchemaField("2/map/ref")	
-	public MapSchema<ColorMeta> colorMeta = new MapSchema<>(ColorMeta.class);
+    @SchemaField("2/map/ref")
+    public MapSchema<ColorMeta> colorMeta = new MapSchema<>(ColorMeta.class);
 
-	@SchemaField("3/boolean")	
-	public boolean started = false;
+    @SchemaField("3/boolean")
+    public boolean started = false;
 
-	@SchemaField("4/boolean")	
-	public boolean ended = false;
+    @SchemaField("4/boolean")
+    public boolean ended = false;
 
-	@SchemaField("5/int64")	
-	public long startTime = 0;
+    @SchemaField("5/int64")
+    public long startTime = 0;
 
-	@SchemaField("6/int64")	
-	public long endTime = 0;
+    @SchemaField("6/int64")
+    public long endTime = 0;
+
+    @Override
+    public MyState _clone() {
+        System.out.println("I'm getting called! that's a good thing");
+        MyState copy = new MyState();
+        copy.cells = (MapSchema<Cell>) cells._clone();
+        copy.players = (MapSchema<Player>) players._clone();
+        copy.colorMeta = (MapSchema<ColorMeta>) colorMeta._clone();
+        copy.started = started;
+        copy.ended = ended;
+        copy.startTime = startTime;
+        copy.endTime = endTime;
+        return copy;
+    }
 }
 
