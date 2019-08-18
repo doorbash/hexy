37255619129008ce4f7e868974c5229ede09fe53
Milad Doorbash <milad.doorbash@gmail.com>
2019-08-18 11:18:08 +0430

    add TextUtils

---
+++ core/src/ir/doorbash/hexy/util/TextUtil.java
@@ -0,0 +1,19 @@
+package ir.doorbash.hexy.util;
+
+/**
+ * Created by Milad Doorbash on 8/18/2019.
+ */
+public class TextUtil {
+    public static String padLeftZeros(String inputString, int length) {
+        if (inputString.length() >= length) {
+            return inputString;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < length - inputString.length()) {
+            sb.append('0');
+        }
+        sb.append(inputString);
+
+        return sb.toString();
+    }
+}
