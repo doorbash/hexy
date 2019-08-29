25c1d36ebc4e191c3d3c1c24fec41ecf72b0298f
Milad Doorbash <milad.doorbash@gmail.com>
2019-08-29 05:12:27 +0430

    add settings dialog

---
+++ android/src/ir/doorbash/hexy/SettingsDialog.java
@@ -0,0 +1,40 @@
+package ir.doorbash.hexy;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Spinner;
+
+/**
+ * Created by Milad Doorbash on 8/28/2019.
+ */
+public class SettingsDialog extends Dialog {
+    Context context;
+
+    Spinner languageSpinner;
+    LanguageSpinnerAdapter languageAdapter;
+
+    public SettingsDialog(Context context) {
+        super(context);
+        this.context = context;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+
+        setContentView(R.layout.dialog_settings);
+
+//        WindowManager.LayoutParams params = getWindow().getAttributes();
+//        params.height = WindowManager.LayoutParams.FILL_PARENT;
+//        getWindow().setAttributes(params);
+
+        languageAdapter = new LanguageSpinnerAdapter(context);
+        languageSpinner = findViewById(R.id.language_spinner);
+        languageSpinner.setAdapter(languageAdapter);
+    }
+}
