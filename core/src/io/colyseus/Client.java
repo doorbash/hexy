f5268d1ca3dc5d34d9faa1263b714011c4ec1e5d
Milad Doorbash <milad.doorbash@gmail.com>
2019-08-29 05:14:52 +0430

    fix body not being sent when options is null

--- core/src/io/colyseus/Client.java
+++ core/src/io/colyseus/Client.java
@@ -120,12 +120,12 @@ public class Client {
 
     private <T extends Schema> void createMatchMakeRequest(String method, String roomName, LinkedHashMap<String, Object> options, Class<T> rootType, Map<String, String> httpHeaders, Map<String, String> wsHeaders, RoomCallback callback, OnError onError) {
         try {
-            String url = endpoint.replace("ws", "http") + "/matchmake/" + method + "/" + roomName;
+            String url = endpoint.replace("ws", "http") + "/matchmake/" + method + "/" + URLEncoder.encode(roomName, "UTF-8");
 
-            String body = null;
+            String body;
             if (options != null) {
                 body = objectMapper.writeValueAsString(objectMapper.convertValue(options, JsonNode.class));
-            }
+            } else body = "{}";
 
             if (httpHeaders == null) httpHeaders = new LinkedHashMap<>();
 
