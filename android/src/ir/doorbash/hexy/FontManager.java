361ce321d56b41162c1e141d59953ca9c815d780
Milad Doorbash <milad.doorbash@gmail.com>
2019-08-29 05:17:08 +0430

    add font manager

---
+++ android/src/ir/doorbash/hexy/FontManager.java
@@ -0,0 +1,27 @@
+package ir.doorbash.hexy;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+/**
+ * Created by Milad Doorbash on 12/26/2016.
+ */
+public class FontManager {
+
+    Typeface noto;
+    Typeface arialbd;
+
+    private static FontManager instance;
+
+    public static FontManager getInstance(Context context) {
+        if (instance == null) {
+            instance = new FontManager(context);
+        }
+        return instance;
+    }
+
+    private FontManager(Context context) {
+        noto = Typeface.createFromAsset(context.getAssets(), "fonts/NotoSans-Regular.ttf");
+        arialbd = Typeface.createFromAsset(context.getAssets(), "fonts/arialbd.ttf");
+    }
+}
