3ccfcff14776797f3ae5c11655f1599fddcd9232
Milad Doorbash <milad.doorbash@gmail.com>
2019-08-29 05:14:21 +0430

    add multi dex

---
+++ android/src/ir/doorbash/hexy/MainApplication.java
@@ -0,0 +1,10 @@
+package ir.doorbash.hexy;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by Milad Doorbash on 8/25/2019.
+ */
+public class MainApplication extends MultiDexApplication {
+
+}
