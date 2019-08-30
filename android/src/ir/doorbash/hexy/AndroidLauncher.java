3d59ed06f30a620ff412e230e9b1aa7d86250262
Milad Doorbash <milad.doorbash@gmail.com>
2019-08-30 22:45:45 +0430

    add empty onbackpressed

--- android/src/ir/doorbash/hexy/AndroidLauncher.java
+++ android/src/ir/doorbash/hexy/AndroidLauncher.java
@@ -6,10 +6,16 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new HexyGame(), config);
-	}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new HexyGame(), config);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
 }
