7bc618faf8ae7db5bc64b240d054c399fbd5c8b4
Milad Doorbash <milad.doorbash@gmail.com>
2019-08-30 22:44:10 +0430

    move Constants to util package

--- core/src/ir/doorbash/hexy/shared/Constants.java
+++
@@ -1,7 +0,0 @@
-package ir.doorbash.hexy.shared;
-
-/**
- * Created by Milad Doorbash on 7/19/2019.
- */
-public class Constants {
-}
---
+++ core/src/ir/doorbash/hexy/util/Constants.java
@@ -0,0 +1,14 @@
+package ir.doorbash.hexy.util;
+
+/**
+ * Created by Milad Doorbash on 8/30/2019.
+ */
+public class Constants {
+    public static final String PREFS_NAME = "settings";
+
+    public static final String DEFAULT_SETTINGS_LANGUAGE = "english";
+
+    public static final String KEY_SETTINGS_SOUND = "settings_sound";
+    public static final String KEY_SETTINGS_LANGUAGE = "settings_language";
+    public static final String KEY_PLAYER_NAME = "player_name";
+}
