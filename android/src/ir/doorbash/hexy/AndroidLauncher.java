9ad96f4e95ce8185ab31da1604f996ec2f3a1b4f
Milad Doorbash <milad.doorbash@gmail.com>
2019-08-31 08:56:57 +0430

    add android configs

--- android/src/ir/doorbash/hexy/AndroidLauncher.java
+++ android/src/ir/doorbash/hexy/AndroidLauncher.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
+import ir.doorbash.hexy.util.Constants;
+import ir.doorbash.hexy.util.Shared;
+
 public class AndroidLauncher extends AndroidApplication {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useWakelock = true;
+        config.disableAudio = !Shared.getInstance(this).getBoolean(Constants.KEY_SETTINGS_SOUND, true);
+        config.hideStatusBar = true;
         initialize(new HexyGame(), config);
     }
 
