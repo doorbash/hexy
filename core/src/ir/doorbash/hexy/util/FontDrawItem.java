0c95b62fffbebb91fdf19e3011058c22da6ee4fc
Milad Doorbash <milad.doorbash@gmail.com>
2019-08-31 08:59:18 +0430

    add fdi

---
+++ core/src/ir/doorbash/hexy/util/FontDrawItem.java
@@ -0,0 +1,14 @@
+package ir.doorbash.hexy.util;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
+/**
+ * Created by Milad Doorbash on 8/31/2019.
+ */
+public class FontDrawItem {
+    public Color color;
+    public String text;
+    public float x;
+    public float y;
+}
