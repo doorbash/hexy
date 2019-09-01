7f351f25b8ae904befa198423cecfd298f89188b
Milad Doorbash <milad.doorbash@gmail.com>
2019-09-01 07:53:07 +0430

    add Control constants

--- core/src/ir/doorbash/hexy/util/Constants.java
+++ core/src/ir/doorbash/hexy/util/Constants.java
@@ -4,11 +4,21 @@ package ir.doorbash.hexy.util;
  * Created by Milad Doorbash on 8/30/2019.
  */
 public class Constants {
+    public static final int CONTROL_TOUCH = 1;
+    public static final int CONTROL_FLOATING = 2;
+    public static final int CONTROL_FIXED_LEFT = 3;
+    public static final int CONTROL_FIXED_RIGHT = 4;
+    public static final int CONTROL_GYRO = 5;
+
     public static final String PREFS_NAME = "settings";
 
-    public static final String DEFAULT_SETTINGS_LANGUAGE = "english";
+    public static final String DEFAULT_SETTINGS_LANGUAGE = "English";
+    public static final String DEFAULT_SETTINGS_GRAPHICS = "high";
+    public static final int DEFAULT_SETTINGS_CONTROL = CONTROL_TOUCH;
 
     public static final String KEY_SETTINGS_SOUND = "settings_sound";
+    public static final String KEY_SETTINGS_GRAPHICS = "settings_graphics";
     public static final String KEY_SETTINGS_LANGUAGE = "settings_language";
+    public static final String KEY_SETTINGS_CONTROL = "settings_control";
     public static final String KEY_PLAYER_NAME = "player_name";
 }
