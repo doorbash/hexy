0bd911eac32c36227716f6c0a6941476379116e6
Milad Doorbash <milad.doorbash@gmail.com>
2019-09-03 06:14:36 +0430

    set config based on control

--- android/src/ir/doorbash/hexy/AndroidLauncher.java
+++ android/src/ir/doorbash/hexy/AndroidLauncher.java
@@ -17,6 +17,15 @@ public class AndroidLauncher extends AndroidApplication {
         config.useWakelock = true;
         config.disableAudio = !Shared.getInstance(this).getBoolean(Constants.KEY_SETTINGS_SOUND, true);
         config.hideStatusBar = true;
+        if (Shared.getInstance(this).getInt(Constants.KEY_SETTINGS_CONTROL, Constants.DEFAULT_SETTINGS_CONTROL) == Constants.CONTROL_DEVICE_ROTATION) {
+            config.useRotationVectorSensor = true;
+            config.useAccelerometer = true;
+            config.useCompass = true;
+        } else {
+            config.useRotationVectorSensor = false;
+            config.useAccelerometer = false;
+            config.useCompass = false;
+        }
         initialize(new HexyGame(), config);
     }
 
