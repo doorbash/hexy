7898729541846786f923f58789a64e960f94803f
Milad Doorbash <milad.doorbash@gmail.com>
2019-10-12 11:36:16 +0330

    add more keys

--- core/src/ir/doorbash/hexy/util/Constants.java
+++ core/src/ir/doorbash/hexy/util/Constants.java
@@ -21,4 +21,7 @@ public class Constants {
     public static final String KEY_SETTINGS_LANGUAGE = "settings_language";
     public static final String KEY_SETTINGS_CONTROL = "settings_control";
     public static final String KEY_PLAYER_NAME = "player_name";
+    public static final String KEY_ID = "id";
+    public static final String KEY_SELECTED_COLOR = "selected_color";
+    public static final String KEY_SELECTED_FILL = "selected_fill";
 }
