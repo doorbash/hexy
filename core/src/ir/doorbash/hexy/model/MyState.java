1f568a5341e17d7d65e5cb9010257469c5840de8
Milad Doorbash <milad.doorbash@gmail.com>
2019-10-16 14:47:33 +0330

    add items

--- core/src/ir/doorbash/hexy/model/MyState.java
+++ core/src/ir/doorbash/hexy/model/MyState.java
@@ -15,22 +15,26 @@ public class MyState extends Schema {
     @SchemaField("0/map/ref")
     public MapSchema<Player> players = new MapSchema<>(Player.class);
 
-    @SchemaField("1/boolean")
-    public boolean started = false;
+    @SchemaField("1/map/ref")
+    public MapSchema<Item> items = new MapSchema<>(Item.class);
 
     @SchemaField("2/boolean")
+    public boolean started = false;
+
+    @SchemaField("3/boolean")
     public boolean ended = false;
 
-    @SchemaField("3/int64")
+    @SchemaField("4/int64")
     public long startTime = 0;
 
-    @SchemaField("4/int64")
+    @SchemaField("5/int64")
     public long endTime = 0;
 
     @Override
     public MyState _clone() {
         MyState copy = new MyState();
         copy.players = (MapSchema<Player>) players._clone();
+        copy.items = (MapSchema<Item>) items._clone();
         copy.started = started;
         copy.ended = ended;
         copy.startTime = startTime;
