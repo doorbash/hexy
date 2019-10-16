a4af93419bf9153f0bc385f2e2d5c356e12e3e23
Milad Doorbash <milad.doorbash@gmail.com>
2019-10-16 14:46:00 +0330

    add coins constant

--- core/src/ir/doorbash/hexy/util/Constants.java
+++ core/src/ir/doorbash/hexy/util/Constants.java
@@ -24,4 +24,5 @@ public class Constants {
     public static final String KEY_ID = "id";
     public static final String KEY_SELECTED_COLOR = "selected_color";
     public static final String KEY_SELECTED_FILL = "selected_fill";
+    public static final String KEY_COINS = "coins";
 }
