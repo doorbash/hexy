fedeed12295b7a74bf3e19962de5780cbb5fa4d2
Milad Doorbash <milad.doorbash@gmail.com>
2019-10-17 17:00:49 +0330

    hide key bar

--- android/src/ir/doorbash/hexy/MainMenuActivity.java
+++ android/src/ir/doorbash/hexy/MainMenuActivity.java
@@ -144,4 +144,18 @@ public class MainMenuActivity extends AppCompatActivity {
     public void openFreeCoinDialog(View view) {
         Toast.makeText(this, "open free coin dialog", Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
 }
